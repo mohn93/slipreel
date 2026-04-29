764ecdf40ee292d15c92233a1f63d8afe7fa5d00
Mohanned Binmiskeen <mohn93@gmail.com>
2026-04-29 13:40:45 +0200

    build(macos): refresh main app Podfile.lock to pick up region capture sources

--- packages/screen_recorder/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/screen_recorder/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,13 @@ import Foundation
 import path_provider_foundation
 import screen_recorder_macos
 import shared_preferences_foundation
+import url_launcher_macos
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRecorderMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRecorderMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
