2b12a979d070b282cc51eefbd26131eab7d5d725
Mohanned Binmiskeen <mohn93@gmail.com>
2026-06-01 22:03:12 +0200

    feat(toolbar): wire TimelineScaleSlider into CanvasToolbar

--- packages/screen_recorder/lib/ui/screens/playback_screen.dart
+++ packages/screen_recorder/lib/ui/screens/playback_screen.dart
@@ -31,6 +31,7 @@ import 'package:screen_recorder/ui/widgets/zoom/playback_canvas.dart';
 import 'package:screen_recorder/state/zoom_preview_override.dart';
 import 'package:screen_recorder/ui/widgets/canvas_toolbar/aspect_ratio_picker.dart';
 import 'package:screen_recorder/ui/widgets/canvas_toolbar/canvas_toolbar.dart';
+import 'package:screen_recorder/ui/widgets/canvas_toolbar/timeline_scale_slider.dart';
 import 'package:screen_recorder/ui/widgets/zoom/zoom_focal_debug_painter.dart';
 import 'package:screen_recorder/ui/widgets/export_dialog/export_dialog.dart';
 import 'package:screen_recorder/ui/screens/settings_screen.dart';
@@ -942,6 +943,10 @@ class _PlaybackScreenState extends ConsumerState<PlaybackScreen>
                                 onChanged: (v) => ref
                                     .read(editorProjectControllerProvider.notifier)
                                     .setOutputAspect(v),
+                              ),
+                              TimelineScaleSlider(
+                                playheadPosition: _smoothPlayhead?.position ??
+                                    _controller.value.position,
                               ),
                             ]),
                             Expanded(
