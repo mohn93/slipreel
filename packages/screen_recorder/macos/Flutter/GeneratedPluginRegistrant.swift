fdbd5e78fd14f30c55dcca1fcac6e681f2d7c577
Mohanned Binmiskeen <mohn93@gmail.com>
2026-06-19 16:57:02 +0200

    build(settings): register package_info_plus macOS plugin (#2)

--- packages/screen_recorder/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/screen_recorder/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import package_info_plus
 import path_provider_foundation
 import screen_recorder_macos
 import shared_preferences_foundation
@@ -14,6 +15,7 @@ import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRecorderMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRecorderMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
